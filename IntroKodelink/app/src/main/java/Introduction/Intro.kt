251f8a2cd59715e70f5466c161354cee4a925de8
ValentinaCorreaG <91170440+ValentinaCorreaG@users.noreply.github.com>
2024-02-08 19:17:25 -0500

    "clase de inicio"

---
+++ IntroKodelink/app/src/main/java/Introduction/Intro.kt
@@ -0,0 +1,17 @@
+package Introduction
+
+class Intro {
+
+}
+
+fun main(){
+    println("Hello word")
+    println("alo")
+    val name:String="Valentina"
+    //
+    println("el nombre es $name \n")
+    println("Ingrese su edad")
+    var edad=readLine()!!.toInt()
+    //!! parcearlo
+    println("la edad es $edad \n")
+}
